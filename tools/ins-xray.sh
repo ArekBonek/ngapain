91756f2a82621a6d29376344a2f4933e01e468f1
ArekBonek <139424279+ArekBonek@users.noreply.github.com>
2023-07-13 11:52:14 +0700

    Update ins-xray.sh

--- tools/ins-xray.sh
+++ tools/ins-xray.sh
@@ -101,9 +101,9 @@ apt install -y nginx
 cd
 rm -fr /etc/nginx/sites-enabled/default
 rm -fr /etc/nginx/sites-available/default
-wget -q -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/FunnyVPN/sc/main/tools/nginx.conf" 
+wget -q -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/ArekBonek/ngapain/main/tools/nginx.conf" 
 #mkdir -p /home/vps/public_html
-wget -q -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/FunnyVPN/sc/main/tools/vps.conf"
+wget -q -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/ArekBonek/ngapain/main/tools/vps.conf"
 
 # Install Xray #
 #==========#
